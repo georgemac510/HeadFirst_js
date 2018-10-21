5e389ad293d225901fb2f7bd06ac7c8a98e48b35
John McLaughlin <georgemac510@gmail.com>
2018-10-21 19:25:46 -0400

    First commit

---
+++ Circle/circle.js
@@ -0,0 +1,15 @@
+function calculateArea(r) {
+  var area;
+  if (r <= 0) {
+    return 0;
+  } else {
+    area = Math.PI * r * r;
+    return area;
+  }
+}
+
+var radius =5.2;
+
+var theArea = calculateArea(radius);
+
+console.log("The area is: " + theArea);
---
+++ app.js
@@ -0,0 +1,6 @@
+var scoops = 5;
+while (scoops> 0) {
+  document.write("another scoop!<br>");
+  scoops = scoops - 1
+}
+document.write("Life without ice cream isn't the same");
