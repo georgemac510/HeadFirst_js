b706fc833e526d3967b80b9627ea171c850f72c3
John McLaughlin <georgemac510@gmail.com>
2018-11-07 19:32:47 -0500

    Add existing files

---
+++ lab/lab.js
@@ -0,0 +1,23 @@
+var test1 = "abcdef";
+var test2 = 123;
+var test3 = true;
+var test4 = {};
+var test5 = [];
+var test6;
+var test7 = {"abcdef": 123};
+var test8 = ["abcdef", 123];
+function test9() {return "abcdef"};
+var test10 = null;
+var test11 = 0/ 0;
+
+console.log(typeof test1);
+console.log(typeof test2);
+console.log(typeof test3);
+console.log(typeof test4);
+console.log(typeof test5);
+console.log(typeof test6);
+console.log(typeof test7);
+console.log(typeof test8);
+console.log(typeof test9);
+console.log(typeof test10);
+console.log(typeof test11);
