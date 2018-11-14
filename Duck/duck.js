9f590d24bbf2c1d40ea13ef6361ca20f1cfcb022
John McLaughlin <georgemac510@gmail.com>
2018-11-14 17:58:26 -0500

    Add existing file

---
+++ Duck/duck.js
@@ -0,0 +1,11 @@
+function Duck(sound) {
+    this.sound = sound;
+    this.quack = function() {console.log(this.sound);}
+}
+
+var toy = new Duck("quack quack");
+
+toy.quack();
+
+console.log(typeof toy);
+console.log(toy instanceof Duck);
