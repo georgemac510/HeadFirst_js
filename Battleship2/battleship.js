0f6742e7d2bc87262da9bdaf80a52f7b74a46d2c
georgemac510 <33005672+georgemac510@users.noreply.github.com>
2018-11-17 18:45:49 -0500

    Update battleship.js

---
+++ Battleship2/battleship.js
@@ -0,0 +1,14 @@
+var view = {
+  displayMessage: function(msg) {
+      var messageArea = document.getElementById("messageArea");
+      messageArea.innerHTML = msg;
+  },
+  displayHit: function(location) {
+      var cell = document.getElementById(location);
+      cell.setAttribute("class", "hit");
+  },
+  displayMiss: function(location) {
+      var cell = document.getElementById(location);
+      cell.setAttribute("class", "miss");
+  }
+};
