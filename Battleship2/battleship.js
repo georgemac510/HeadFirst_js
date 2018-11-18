d6d2a1d016809ca85d424e833a0f20ad2a5f6d50
georgemac510 <33005672+georgemac510@users.noreply.github.com>
2018-11-17 19:40:46 -0500

    Add existing file

--- Battleship2/battleship.js
+++ Battleship2/battleship.js
@@ -12,3 +12,11 @@ var view = {
       cell.setAttribute("class", "miss");
   }
 };
+
+view.displayMiss("00");
+view.displayHit("34");
+view.displayMiss("55");
+view.displayHit("12");
+view.displayMiss("25");
+view.displayHit("26");
+view.displayMessage("Tap tap, is this thing on?");
